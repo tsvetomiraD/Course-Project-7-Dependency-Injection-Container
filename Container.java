d405e6564f74ee202b28a6570a3aed86c2b18b93
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2023-01-13 09:42:59 +0200

    Update Container.java

--- Container.java
+++ Container.java
@@ -1,35 +1,133 @@
 package dic;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Container {
+    private final Set<Class<?>> PRIMITIVE_TYPES = Set.of(byte.class, short.class, int.class, long.class, float.class, double.class, char.class, Character.class, String.class, boolean.class, Integer.class);
     Map<String, Object> classes = new HashMap<>();
+    Map<Class<?>, Class<?>> impls = new HashMap<>();
+    List<Class<?>> startedClass = new ArrayList<>();
+    Properties properties;
+
+    public Container(Properties properties) {
+        this.properties = properties;
+    }
 
     public Object getInstance(String key) {
-        return null;
+        return classes.get(key);
     }
 
     public <T> T getInstance(Class<T> c) throws Exception {
         if (classes.containsKey(c.getName())) {
             return (T) classes.get(c.getName());
         }
-        Object cl = c.getDeclaredConstructor().newInstance();
-        Field[] fields = c.getDeclaredFields();
-        for (Field f : fields) {
-            f.setAccessible(true);
-            f.set(classes.get(f.getType().getName()), cl);
+
+        Class<?> classImpl = null;
+        if (c.isInterface()) {
+            if (!impls.containsKey(c)) {
+                Default defaultClass = c.getDeclaredAnnotation(Default.class);
+                if (defaultClass != null) {
+                    classImpl = defaultClass.cl();
+                    impls.put(c, classImpl);
+                }
+            }
+
+            if (classImpl == null) {
+                throw new RegistryException("Interface is not implemented");
+            }
         }
-        classes.put(c.getName(), cl);
-        return (T) cl;
+        classImpl = classImpl != null ? classImpl : c;
+        if (classes.containsKey(classImpl.getName())) {
+            return (T) classes.get(classImpl.getName());
+        }
+        if (startedClass.size() == 0) {
+            startedClass.add(c);
+        }
+        Object res = instanceClass(classImpl);
+        classes.put(classImpl.getName(), res);
+        return (T) res;
     }
 
-    public void decorateInstance(Object o) {
-        return;
+    private Object instanceClass(Class<?> classImpl) throws Exception {
+        Constructor<?> con = getConstructor(classImpl);
+        Object inst = null;
+        if (con != null) {
+            Object[] params = getParams(con);
+            inst = con.newInstance(params);
+        } else {
+            inst = classImpl.getDeclaredConstructor().newInstance();
+        }
+        instanceFields(inst);
+
+        if (Initializer.class.isAssignableFrom(classImpl)) {
+            ((Initializer) inst).init();
+        }
+        return inst;
+    }
+
+    private Object[] getParams(Constructor<?> con) throws Exception {
+        Object[] params = null;
+        Class<?>[] paramsTypes = con.getParameterTypes();
+
+        if (paramsTypes.length != 0) {
+            params = new Object[paramsTypes.length];
+        }
+
+        for (int i = 0; i < paramsTypes.length; i++) {
+            Class<?> paramType = paramsTypes[i];
+            Object paramInst = getInstance(paramType);
+            params[i] = paramInst;
+        }
+        return params;
+    }
+
+    private Constructor<?> getConstructor(Class<?> classImpl) {
+        Constructor<?> con = null;
+        for (Constructor c : classImpl.getDeclaredConstructors()) {
+            Inject annotation = c.getDeclaredAnnotation(Inject.class);
+            if (annotation == null) {
+                continue;
+            }
+            c.setAccessible(true);
+            con = c;
+            break;
+        }
+        return con;
+    }
+
+    private void instanceFields(Object cl) throws Exception {
+        Class<?> claasImpl = cl.getClass();
+        for (Field f : claasImpl.getDeclaredFields()) {
+            Inject annotation = f.getAnnotation(Inject.class);
+            if (annotation == null) {
+                continue;
+            }
+            f.setAccessible(true);
+            Named name = f.getAnnotation(Named.class);
+            Object o = null;
+
+            if (PRIMITIVE_TYPES.contains(f.getType())) {
+                o = name != null ? properties.get(name) : properties.get(f.getName());
+            }
+            Class<?> checkClass = startedClass.size() != 0 ? startedClass.get(0) : null;
+            if (f.getType().equals(checkClass)) {
+                throw new RegistryException("Circular dependency");
+            }
+            if (o == null) {
+                if (name != null) {
+                    o = getInstance(f.getName());
+                } else {
+                    o = getInstance(f.getType());
+                }
+            }
+            f.set(cl, o);
+        }
+    }
+
+    public void decorateInstance(Object o) throws Exception {
+        instanceFields(o);
     }
 
     public void registerInstance(String key, Object instance) {
@@ -37,16 +135,7 @@ class Container {
     }
 
     public void registerImplementation(Class c, Class subClass) throws Exception {
-        if (classes.containsKey(c.getName())) {
-            return;
-        }
-        Object cl = c.getDeclaredConstructor().newInstance();
-        Field[] fields = c.getDeclaredFields();
-        for (Field f : fields) {
-            f.setAccessible(true);
-            f.set(getInstance(f.getType()), cl);
-        }
-        classes.put(c.getName(), cl);
+        impls.put(c, subClass);
     }
 
     public void registerImplementation(Class c) throws Exception {
@@ -63,6 +152,6 @@ class Container {
     }
 
     public void registerInstance(Object instance) {
-        classes.put("name", instance);
+        classes.put(instance.getClass().getName(), instance);
     }
 }
